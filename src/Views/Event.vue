4f64cd1e7ebde1f5213f1963b511aec23ef849b1
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2021-09-22 12:19:02 +0200

    Inital commit

---
+++ src/Views/Event.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="event">
+        <!--Oppsett av Grid for Konsert med EventCard.vue-->
+        <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mx-12">
+        <EventCard/>
+        </ul>
+        testing
+
+    </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+</script>
+
+<style>
+
+</style>
